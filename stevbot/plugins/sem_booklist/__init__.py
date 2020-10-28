9cf7806a332e18110e433f702933fce421f2822d
stevfan <liangeuofan@hotmail.com>
2020-10-28 10:56:03 +0800

    0.1.1 发行稳定版

--- stevbot/plugins/sem_booklist/__init__.py
+++ stevbot/plugins/sem_booklist/__init__.py
@@ -23,9 +23,10 @@ booking_reference = on_regex(r'查表', priority=5)
 @booking_main.handle()
 async def handle_received_date(bot: Bot, event: Event, state: dict):
     args = str(event.message).strip()
+    arg_list = args.split()
+    print(len(arg_list))
     # await nonebot.get_bots()['3092531246'].send_group_msg(group_id='142872436', message='发送群消息测试')
-    if args:
-        arg_list = args.split()
+    if len(arg_list) > 1:
         # 设备id参数，没有传入32位id或长度不够则默认14号楼107电镜
         if len(arg_list[1]) == 32:
             state['id'] = arg_list[1]
@@ -33,15 +34,15 @@ async def handle_received_date(bot: Bot, event: Event, state: dict):
             if M2EID.get(arg_list[1]) is not None:
                 state['id'] = M2EID.get(arg_list[1])
             else:
-                await booking_main.reject(f'{arg_list[1]}编号还未加入机器人的内置id列表！')
+                await booking_main.finish(f'{arg_list[1]}编号还未加入机器人的内置id列表！')
         i = 2 if len(arg_list) > 2 else 1
         result = await process_date(arg_list[i])
         if result:
             state['mm'], state['dd'] = result
         else:
-            await booking_main.reject('日期格式不对或月份日数不正确，请确认是否是mmdd格式（如0809）')
+            await booking_main.finish('日期格式不对或月份日数不正确，请确认是否是mmdd格式（如0809）')
     else:
-        await booking_main.reject(r'''- /设备时间 [设备id] 日期
+        await booking_main.finish(r'''- /设备时间|/看看 [设备id] 日期
 - - 设备id：(可选)32位字母数字结构字符串，留空默认填入140107的电镜设备id
 机器人也内置了励吾楼电镜的id，直接输入m1代替设备id即可。
 - - 日期：(必须)请输入mmdd格式的日期，如10月15日则输入1015
@@ -52,7 +53,7 @@ async def handle_received_date(bot: Bot, event: Event, state: dict):
 将会返回10月14日140107电镜的预约列表
 - - /设备时间 m1 明天
 将会返回明天励吾楼场发射电镜的预约列表
-- - /设备时间 一串设备id 后天
+- - /看看 一串设备id 后天
 将会返回给定设备后天的预约列表''')
 
 
